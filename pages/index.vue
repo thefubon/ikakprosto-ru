92a19d8d1535db7f2fc886c729093aa89e4942a8
Fubon <thefubon@gmail.com>
2024-10-22 18:03:38 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-  Главная
+  <main class="container">
+    <NuxtLink class="bg-primary text-white py-2 px-4 inline-flex justify-center items-center rounded-lg" to="/posts">
+      Посты тут
+    </NuxtLink>
+  </main>
 </template>
