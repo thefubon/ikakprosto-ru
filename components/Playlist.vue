c6c09f545e3c4bff5308ecd2a5d373f390bc47e8
Fubon <thefubon@gmail.com>
2024-10-23 18:40:55 +0300

    Изменил <img> на <NuxtImg>

--- components/Playlist.vue
+++ components/Playlist.vue
@@ -5,10 +5,10 @@
       <div v-for="(item, index) in images" :key="index" class="relative rounded-md overflow-hidden">
         <template v-if="item.url && item.url !== '#'">
           <a :href="item.url" target="_blank">
-            <img :src="item.image" alt="Image"
-              class="w-full h-auto rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+            <NuxtImg :src="item.image" alt="Image"
+              class="w-full h-auto rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" />
             <div v-if="item.close" class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-              <img src="/public/key.svg" alt="Icon" class="w-8 h-8">
+              <img src="/public/key.svg" alt="Icon" class="w-8 h-8" />
             </div>
           </a>
         </template>
@@ -16,7 +16,7 @@
           <p>
             <img :src="item.image" alt="Image" class="w-full h-auto rounded-lg shadow-md">
           <div v-if="item.close" class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-            <img src="/public/key.svg" alt="Icon" class="w-8 h-8">
+            <img src="/public/key.svg" alt="Icon" class="w-8 h-8" />
           </div>
           </p>
         </template>
